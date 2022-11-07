493fb83a469a11cef1572e858b997c4a462ed286
nghiaha1 <maxx.minimizee@gmail.com>
2022-11-07 09:52:23 +0700

    update

--- app/src/main/java/t2010a/cookpad_clone/fragment/ProfileFragment.java
+++ app/src/main/java/t2010a/cookpad_clone/fragment/ProfileFragment.java
@@ -4,21 +4,36 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.Locale;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import t2010a.cookpad_clone.R;
 import t2010a.cookpad_clone.local_data.LocalDataManager;
+import t2010a.cookpad_clone.model.LoginResponse;
+import t2010a.cookpad_clone.model.user.UpdateUser;
 import t2010a.cookpad_clone.model.user.User;
+import t2010a.cookpad_clone.repository.Repository;
 
 
 public class ProfileFragment extends Fragment {
-    View itemView;
-    EditText et_profile_username, et_profile_fullName, et_profile_address,
-            et_profile_phone, et_profile_email, et_profile_detail;
+    private View itemView;
+    private EditText etProfileFullName, etProfileAddress,
+            etProfilePhone, etProfileEmail, etProfileDetail;
+    private Button btnUpdateUser;
+    private Repository repository;
+    private User user;
+    private UpdateUser updateUser = new UpdateUser();
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -27,25 +42,64 @@ public class ProfileFragment extends Fragment {
         itemView = inflater.inflate(R.layout.fragment_profile, container, false);
         initView(itemView);
         initData();
+        setBtnUpdateUser();
         return itemView;
     }
 
     private void initView(View itemView) {
-        et_profile_username = itemView.findViewById(R.id.et_profile_username);
-        et_profile_fullName = itemView.findViewById(R.id.et_profile_fullName);
-        et_profile_address = itemView.findViewById(R.id.et_profile_address);
-        et_profile_phone = itemView.findViewById(R.id.et_profile_phone);
-        et_profile_email = itemView.findViewById(R.id.et_profile_email);
-        et_profile_detail = itemView.findViewById(R.id.et_profile_detail);
+        etProfileFullName = itemView.findViewById(R.id.etProfileFullName);
+        etProfileAddress = itemView.findViewById(R.id.etProfileAddress);
+        etProfilePhone = itemView.findViewById(R.id.etProfilePhone);
+        etProfileEmail = itemView.findViewById(R.id.etProfileEmail);
+        etProfileDetail = itemView.findViewById(R.id.etProfileDetail);
+        btnUpdateUser = itemView.findViewById(R.id.btnUpdateUser);
     }
 
     private void initData() {
-        User user = LocalDataManager.getUserDetail();
-        et_profile_username.setText(user.getUsername());
-        et_profile_fullName.setText(user.getFullName());
-        et_profile_address.setText(user.getAddress());
-        et_profile_phone.setText(user.getPhone());
-        et_profile_email.setText(user.getEmail());
-        et_profile_detail.setText(user.getDetail());
+        user = LocalDataManager.getUserDetail();
+        etProfileFullName.setText(user.getFullName());
+        etProfileAddress.setText(user.getAddress());
+        etProfilePhone.setText(user.getPhone());
+        etProfileEmail.setText(user.getEmail());
+        etProfileDetail.setText(user.getDetail());
+
+    }
+
+    private void setBtnUpdateUser() {
+        String accessToken = LocalDataManager.getAccessToken();
+        repository = Repository.getInstance();
+        String fullName = etProfileFullName.getText().toString().toLowerCase(Locale.ROOT).trim();
+//        String phone = etProfilePhone.getText().toString().trim();
+//        String email = etProfileEmail.getText().toString().trim();
+//        String address = etProfileAddress.getText().toString().trim();
+
+        updateUser.setFullName(fullName);
+//        user.setEmail(email);
+//        user.setPhone(phone);
+//        user.setAddress(address);
+
+        btnUpdateUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                repository.getService().updateUser(updateUser, user.getId(), accessToken).enqueue(new Callback<UpdateUser>() {
+                    @Override
+                    public void onResponse(Call<UpdateUser> call, Response<UpdateUser> response) {
+                        if (response.code() == 200) {
+                            Log.d("TAG", "onResponse: change success");
+                            Toast.makeText(getActivity(), "Success", Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            Log.d("TAG", "onResponse:change fail");
+                            Toast.makeText(getActivity(), "Fail", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<UpdateUser> call, Throwable t) {
+
+                    }
+                });
+            }
+        });
     }
 }
